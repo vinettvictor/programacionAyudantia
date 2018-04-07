deb3e02389fa086c67d90dd3132066876005dc27
vinettvictor <32687602+vinettvictor@users.noreply.github.com>
2018-04-06 23:54:58 -0300

    avance 3

---
+++ inventarioObjetos.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoayudantia;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Vinett
+ */
+
+public class inventarioObjetos {
+    private int maxInventario = 10;
+    ArrayList<objetoEquipable> coleccObjetoEquipable = new ArrayList<objetoEquipable>();
+    
+    
+    public void agregarObjeto(){
+        
+       if (coleccObjetoEquipable.size()<maxInventario){
+        coleccObjetoEquipable.add(new objetoEquipable());
+       } else {
+           System.out.println("El inventario esta lleno");
+       }
+    }
+    
+    public void quitarObjeto(int posicion){
+        coleccObjetoEquipable.remove(posicion);
+    }
+    
+    private void buscarObjetoRango(ArrayList<objetoEquipable> obj ,  int rango){
+        boolean flag = false;
+        for (int i = 0;i<coleccObjetoEquipable.size();i++) {
+            if(obj.get(i).getRangoObj() == rango){
+                System.out.println("Objeto: "+coleccObjetoEquipable.get(i).getObjetoSeleccionado()+ "\tRango: "+coleccObjetoEquipable.get(i).getRangoObj());
+                flag = true;
+            } 
+                
+                                                
+        }       
+    }
+    
+    public void mostrarFiltroRango(int rango){
+        if (rango >=1 && rango<=10) {
+            buscarObjetoRango(coleccObjetoEquipable,rango);
+        }else {
+            System.out.println("Por favor asegurese de que el rango que esta ingresando esta dentro de los parametros");
+        }
+    }
+    
+    private void buscarObjetos (ArrayList<objetoEquipable> obj ){
+        for(int i=0;i<coleccObjetoEquipable.size();i++){
+            System.out.println("objeto "+(i+1)+": "+coleccObjetoEquipable.get(i).getObjetoSeleccionado() + 
+                    "\tRango: "+coleccObjetoEquipable.get(i).getRangoObj());
+            
+        }
+    }
+    
+    public void mostrarObjeto(){
+        buscarObjetos(coleccObjetoEquipable);
+    }
+    
+    
+}
---
+++ monstruo.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoayudantia;
+
+import java.util.Random;
+/**
+ *
+ * @author Vinett
+ */
+public class monstruo {
+    Random rnd = new Random();
+    private int hp,atk,def,spd;
+    
+    public monstruo(){
+        this.hp  = rnd.nextInt(500)+3500;
+        this.atk = rnd.nextInt(500)+1000;
+        this.def = rnd.nextInt(20)+5;
+        this.spd = rnd.nextInt(90)+10;        
+    }
+    
+    public int getHp(){
+        return hp;
+    }    
+    public void setHp(int hp){
+        this.hp = hp;
+    }
+    
+    public int getAtk(){
+        return atk;
+    }   
+    public void setAtk(int atk){
+        this.atk = atk;
+    }
+    
+    public int getDef(){
+        return def;
+    }
+    public void setDef(int def){
+        this.def = def;
+    }
+    
+    public int getSpd(){
+        return spd;
+    }
+    public void setSpd(int spd){
+        this.spd = spd;    
+       
+    }
+    
+    private void crearObjetoDropeable(){
+        objetoEquipable objeto = new objetoEquipable();
+        objeto.getObjetoSeleccionado(); //  se llama el nombre del objeto
+        objeto.getObjetoEquipado(); // los stat del objeto que salio 
+        
+        System.out.println("El objeto "+objeto.getObjetoSeleccionado()+" contiene un rango de "+ objeto.getObjetoEquipado());
+    }
+}
